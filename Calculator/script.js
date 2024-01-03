b1d67a9c8c20fc43b337b862567949ff1f9fdbb2
divyesh1012 <divyeshsarvaiya2@gmail.com>
2024-01-03 08:27:41 +0530

    calculator changes done

--- Calculator/script.js
+++ Calculator/script.js
@@ -18,3 +18,21 @@ function calculate(){
         input.value = "Error";
     }
 }
+
+function toggleKey(){
+    // let flag = 0; 
+    // if(flag == 0){
+    //     input.value.unshift("-");
+    //     flag = 1;
+    // }
+    // else if(flag == 1){
+    //     input.value.shift();
+    //     flag = 0;
+    // }
+
+    if (input.value.charAt(0) !== '-') {
+        input.value = '-' + input.value;
+    } else {
+        input.value = input.value.substring(1);
+    }
+}
